c93d4d3344365b835b1c6fb9e706b0c83ec4f8cd
Usman Ali <py.usman.ali@gmail.com>
2025-07-25 22:36:00 +0500

    Initial Commit

---
+++ netflix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  netflix
+//
+//  Created by usman ali on 25/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ netflix/netflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixApp.swift
+//  netflix
+//
+//  Created by usman ali on 25/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct netflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
